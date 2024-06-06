963dfcf72f9c913b5e5917c88d985f324f4b2d18
Matt Kars <mattkars@gmail.com>
2024-06-06 16:16:20 -0400

    removed top linking function

--- src/Pages/Index.jsx
+++ src/Pages/Index.jsx
@@ -33,8 +33,7 @@ const Index = () => {
 
   return (
     <div
-      id="top"
-      className="flex flex-col items-center w-full bg-gradient-to-b from-[#FAF0E5] to-[#D2B48C] h-screen !scroll-smooth"
+      className="flex flex-col items-center w-full bg-gradient-to-b from-[#FAF0E5] to-[#D2B48C] h-screen"
     >
       <img
         src="/images/elimHeader.png"
@@ -46,11 +45,9 @@ const Index = () => {
       </h1>
 
       <div className="flex flex-col sm:flex-row gap-6 sm:gap-10 items-initial w-full sm:bg-[#FCF8F1] sm:px-10 max-w-[1000px] overflow-hidden py-3">
-
         {/* media player */}
         <div className="w-full px-2 sm:px-0 flex-1">
           <iframe
-            id="vid"
             className="w-full aspect-16/9 rounded-lg sm:rounded-xl sm:px-0"
             src={currentVid}
             title="YouTube video player"
@@ -65,23 +62,21 @@ const Index = () => {
         <ul className="flex flex-col items-center sm:items-start overflow-y-scroll gap-2 px-2 flex-initial">
           {links.map((el, ind) => {
             return (
-              <a className="w-full" href="#top">
-                <li
-                  key={ind}
-                  className="cursor-pointer font-semibold whitespace-nowrap pr-4 text-2xl sm:text-lg p-4 rounded-md hover:!bg-[#a2dbc2] w-full"
-                  onClick={(evt) => {
-                    setCurrentVid(
-                      `https://www.youtube.com/embed/${el.href}?si=yZ4Vp14zKTVIpxV0`
-                    );
-                    handleSelected(ind);
-                  }}
-                  style={{
-                    background: el.selected ? "#8CD2B4" : "#8CAAD2",
-                  }}
-                >
-                  {el.date}
-                </li>
-              </a>
+              <li
+                key={ind}
+                className="cursor-pointer font-semibold whitespace-nowrap pr-4 text-2xl sm:text-lg p-4 rounded-md hover:!bg-[#a2dbc2] w-full"
+                onClick={(evt) => {
+                  setCurrentVid(
+                    `https://www.youtube.com/embed/${el.href}?si=yZ4Vp14zKTVIpxV0`
+                  );
+                  handleSelected(ind);
+                }}
+                style={{
+                  background: el.selected ? "#8CD2B4" : "#8CAAD2",
+                }}
+              >
+                {el.date}
+              </li>
             );
           })}
         </ul>
